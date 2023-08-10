95a9ae170ad78640fa8d6c3013963d72e7cd71dc
miohitokiri5474 <lltzpp>
2023-08-10 21:42:30 +0800

    style(main.py): modify the code according to pylint to comply with the PEP8 specification
    Modify the code (main.py) according to pylint to comply with the PEP8 specification.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import os
+"""
+This is the main file for this project, url shortener.
+It is power by FastAPI, jose, pydantic, passlib.
+"""
 import random
-import sqlite3
 from datetime import datetime, timedelta
-from typing import Annotated, Union
+from typing import Union
 
-import bcrypt
-from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
@@ -20,15 +21,21 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 class URLAdminInfo(BaseModel):
+    """the BaseModel of url admin information"""
+
     original_url: str
     admin_url: str
 
 
 class URLInfo(URLAdminInfo):
+    """with clicks"""
+
     clicks: int
 
 
 class User(BaseModel):
+    """the BaseModel of user information"""
+
     username: str
     passwd: bytes
     full_name: Union[str, None] = None
@@ -37,11 +44,15 @@ class User(BaseModel):
 
 
 class Token(BaseModel):
+    """the BaseModel of JWT token"""
+
     access_token: str
     token_type: str
 
 
 class TokenData(BaseModel):
+    """the BaseModel of user data"""
+
     username: Union[str, None] = None
 
 
@@ -53,19 +64,22 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def raise_bad_request(message):
+    """raise bad request"""
     raise HTTPException(status_code=400, detail=message)
 
 
 def create_random_url() -> str:
+    """create a random url shortener admin code if user did not provide one"""
     res = ""
     lib = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     length = 8
-    for i in range(8):
+    for _ in range(length):
         res += lib[random.randint(0, len(lib))]
     return res
 
 
 def decode_token(token) -> User:
+    """decode JWT token into user information"""
     user_info = db.get_user_info(token)
     return User(
         username=user_info[0],
@@ -77,24 +91,28 @@ def decode_token(token) -> User:
 
 
 def verify_password(plain_password, hashed_password):
+    """verify password if it is correct or not"""
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password):
+    """get the hashed password"""
     return pwd_context.hash(password)
 
 
 def authenticate_user(username: str, passwd: str):
+    """authenticate user to login"""
     user_info = decode_token(username)
     if not user_info:
-        return False
+        return None
     if not verify_password(passwd, user_info.passwd):
-        return False
+        return None
 
     return user_info
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
+    """get current user information"""
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -106,22 +124,24 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
+    except JWTError as exc:
+        raise credentials_exception from exc
 
     user_info = decode_token(token_data.username)
     if user_info is None:
-        raise credentials_exceptino
+        raise credentials_exception
     return user_info
 
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    """get current active user"""
     if current_user.disable:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    """create JWT token"""
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -142,11 +162,13 @@ if db.check_username_is_available("lltzpp"):
 
 @app.get("/")
 async def read_root():
+    """root"""
     return "Welcome to the URL shortener API"
 
 
 @app.get("/r/{url}")
 async def get_url(url: str):
+    """temporary redirect"""
     if db.check_admin_code_is_available(url):
         return "This admin url is not exist.\n"
 
@@ -157,21 +179,24 @@ async def get_url(url: str):
 
 @app.get("/list/")
 async def list_url(current_user: User = Depends(get_current_active_user)):
+    """list url shortener which is create by current user"""
     return db.list(current_user.username)
 
 
 @app.get("/whoami/", response_model=User)
 async def read_me(current_user: User = Depends(get_current_active_user)):
+    """get current user information"""
     current_user.passwd = b"---"
     return current_user
 
 
 @app.post("/token")
 async def user_login(form_data: OAuth2PasswordRequestForm = Depends()):
+    """user login"""
     user_info = authenticate_user(form_data.username, form_data.password)
 
     if not user_info:
-        raise HTTPEXception(
+        raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username of password",
             headers={"WWW-Authenticate": "Bearer"},
@@ -189,6 +214,7 @@ async def user_login(form_data: OAuth2PasswordRequestForm = Depends()):
 async def adding_url_with_auth_token(
     url: URLAdminInfo, current_user: User = Depends(get_current_active_user)
 ):
+    """add url shortener"""
     if url.admin_url == "":
         url.admin_url = create_random_url()
         while not db.check_admin_code_is_available(url.admin_url):
@@ -207,6 +233,7 @@ async def adding_url_with_auth_token(
 async def delete_url_with_auth_token(
     url: str, current_user: User = Depends(get_current_active_user)
 ):
+    """delete url shortener with admin code"""
     try:
         db.delete_url(url, current_user.username)
         return url + " is successfully deleted.\n"
