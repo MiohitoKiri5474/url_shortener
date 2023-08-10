b3ebb56cf9506bf5f4232d2a1c679931548c2c81
miohitokiri5474 <lltzpp>
2023-08-10 19:39:51 +0800

    update

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 import sqlite3, os
 
-url_db_path = "url_shortener.db"
+db_path = "url_shortener.db"
 
 def build_db():
-    if os.path.exists (url_db_path):
+    if os.path.exists (db_path):
         return
-    conn = sqlite3.connect (url_db_path)
+    conn = sqlite3.connect (db_path)
     cursor = conn.cursor()
     cursor.execute ('''CREATE TABLE IF NOT EXISTS url_mapping (code TEXT PRIMARY KEY, url TEXT, username TEXT, clicks INTEGER DEFAULT 0)''')
     conn.commit()
@@ -14,7 +14,7 @@ def build_db():
     conn.close()
 
 def check_admin_code_is_available (admin_url: str):
-    conn = sqlite3.connect (url_db_path)
+    conn = sqlite3.connect (db_path)
     cursor = conn.cursor()
     cursor.execute ('''SELECT code FROM url_mapping WHERE code = ?''', (admin_url,))
     result = cursor.fetchone()
@@ -26,21 +26,21 @@ def insert_url (admin_url: str, ori: str, username: str):
     if not check_admin_code_is_available (admin_url):
         raise ValueError ("The chosen short code is already taken. Please choose a different one.")
 
-    conn = sqlite3.connect (url_db_path)
+    conn = sqlite3.connect (db_path)
     cursor = conn.cursor()
     cursor.execute ('''INSERT INTO url_mapping (code, url, username) VALUES (?, ?, ?)''', (admin_url, ori, username))
     conn.commit()
     conn.close()
 
 def update_click_count (admin_url: str):
-    conn = sqlite3.connect (url_db_path)
+    conn = sqlite3.connect (db_path)
     cursor = conn.cursor()
     cursor.execute ('''UPDATE url_mapping SET clicks = clicks + ? WHERE code = ?''', (1, admin_url))
     conn.commit()
     conn.close()
 
 def query (url: str):
-    conn = sqlite3.connect (url_db_path)
+    conn = sqlite3.connect (db_path)
     cursor = conn.cursor()
     cursor.execute ('''SELECT url FROM url_mapping WHERE code = ?''', (url,))
     result = cursor.fetchone()
@@ -54,7 +54,7 @@ def delete_url (url: str, username: str):
     if check_admin_code_is_available (url):
         raise ValueError ("This admin url is not exist.\n")
 
-    conn = sqlite3.connect (url_db_path)
+    conn = sqlite3.connect (db_path)
     cursor = conn.cursor()
     cursor.execute ('''SELECT url FROM url_mapping WHERE code = ? AND username = ?''', (url, username))
     result = cursor.fetchone()
@@ -65,10 +65,62 @@ def delete_url (url: str, username: str):
     conn.close()
 
 def list (username: str):
-    conn = sqlite3.connect (url_db_path)
+    conn = sqlite3.connect (db_path)
     cursor = conn.cursor()
     cursor.execute ('''SELECT code, url, clicks FROM url_mapping WHERE username = ?''', (username,))
     all_mappings = cursor.fetchall()
     cursor.close()
 
     return all_mappings
+
+def check_username_is_available (name: str):
+    conn = sqlite3.connect (db_path)
+    cursor = conn.cursor()
+    cursor.execute ('''SELECT name FROM user_mapping WHERE name = ?''', (name,))
+    result = cursor.fetchone()
+    conn.close()
+
+    return result is None
+
+def insert_user (name: str, passwd, full_name: str, email: str):
+    if not check_username_is_available (name):
+        raise ValueError ("The chosen user name is alreadt taken, Please choose a different one.")
+
+    conn = sqlite3.connect (db_path)
+    cursor = conn.cursor()
+    cursor.execute ('''INSERT INTO user_mapping (name, passwd, full_name, email) VALUES (?, ?, ?, ?)''', (name, passwd, full_name, email))
+    conn.commit()
+    conn.close()
+
+def update_user_status (name: str, disable: bool):
+    conn = sqlite3.connect (db_path)
+    cursor = conn.cursor()
+    cursor.execute ('''UPDATE user_mapping SET disable = ? WHERE name = ?''', (disable, name))
+    conn.commit()
+    conn.close()
+
+def get_passwd (name: str):
+    if check_username_is_available (name):
+        raise ValueError ("User not found.")
+
+    try:
+        result = get_user_info (name)[1]
+        if result:
+            return result
+        raise ValueError ("User information not found.")
+    except ValueError as error:
+        raise ValueError (str(error))
+
+def get_user_info (name: str):
+    if check_username_is_available (name):
+        raise ValueError ("User not found.")
+
+    conn = sqlite3.connect (db_path)
+    cursor = conn.cursor()
+    cursor.execute ('''SELECT name, passwd, full_name, email, disable FROM user_mapping WHERE name = ?''', (name,))
+    result = cursor.fetchall()
+    conn.close()
+
+    if result:
+        return result[0]
+    raise ValueError ("User information not found.")
--- main.py
+++ main.py
@@ -1,68 +1,80 @@
-from fastapi import FastAPI, Depends, HTTPException, Request, status
+import os
+import random
+import sqlite3
+from datetime import datetime, timedelta
+from typing import Annotated, Union
+
+import bcrypt
+from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-from pydantic import BaseModel
-from jose import jwt, JWTError
+from jose import JWTError, jwt
 from passlib.context import CryptContext
-from typing import Annotated, Union
-from datetime import datetime, timedelta
+from pydantic import BaseModel
 
-import random, sqlite3, os, bcrypt
-
-import db, db
+import db
 
 SECRET_KEY = "7de99a859d0920dfb46628ab5af61dad0d618072863c2005e22cf06390639ca3"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-class URLAdminInfo (BaseModel):
+
+class URLAdminInfo(BaseModel):
     original_url: str
     admin_url: str
 
-class URLInfo (URLAdminInfo):
+
+class URLInfo(URLAdminInfo):
     clicks: int
 
-class User (BaseModel):
+
+class User(BaseModel):
     username: str
     passwd: bytes
     full_name: Union[str, None] = None
     email: Union[str, None] = None
     disable: Union[bool, None] = None
 
-class Token (BaseModel):
+
+class Token(BaseModel):
     access_token: str
     token_type: str
 
-class TokenData (BaseModel):
+
+class TokenData(BaseModel):
     username: Union[str, None] = None
+
 
 db.build_db()
 app = FastAPI()
 
-pwd_context =  CryptContext (schemes = ["bcrypt"], deprecated = "auto")
-oauth2_scheme = OAuth2PasswordBearer (tokenUrl = "token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def raise_bad_request (message):
-    raise HTTPException (status_code = 400, detail = message)
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
 
 def create_random_url() -> str:
     res = ""
     lib = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     length = 8
-    for i in range (8):
-        res += lib[random.randint (0, len (lib))]
+    for i in range(8):
+        res += lib[random.randint(0, len(lib))]
     return res
 
-def decode_token (token) -> User:
-    user_info = db.get_user_info (token)
-    return User (
-            username = user_info[0],
-            passwd = user_info[1],
-            full_name = user_info[2],
-            email = user_info[3],
-            disable = user_info[4]
+
+def decode_token(token) -> User:
+    user_info = db.get_user_info(token)
+    return User(
+        username=user_info[0],
+        passwd=user_info[1],
+        full_name=user_info[2],
+        email=user_info[3],
+        disable=user_info[4],
     )
+
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -71,119 +83,138 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def authenticate_user (username: str, passwd: str):
-    user_info = decode_token (username)
+
+def authenticate_user(username: str, passwd: str):
+    user_info = decode_token(username)
     if not user_info:
         return False
-    if not verify_password (passwd, user_info.passwd):
+    if not verify_password(passwd, user_info.passwd):
         return False
 
     return user_info
 
-async def get_current_user (token: str = Depends (oauth2_scheme)):
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode (token, SECRET_KEY, algorithms = [ALGORITHM])
-        username: str = payload.get ("sub")
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData (username = username)
+        token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
 
-    user_info = decode_token (token_data.username)
+    user_info = decode_token(token_data.username)
     if user_info is None:
         raise credentials_exceptino
     return user_info
 
-async def get_current_active_user (current_user: User = Depends (get_current_user)):
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
     if current_user.disable:
-        raise HTTPException (status_code = 400, detail = "Inactive user")
+        raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-def create_access_token (data: dict, expires_delta: Union[timedelta, None] = None):
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta (minutes = 15)
-    to_encode.update ({"exp": expire})
-    encode_jwt = jwt.encode (to_encode, SECRET_KEY, algorithm = ALGORITHM)
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encode_jwt
 
-if db.check_username_is_available ('admin'):
-    db.insert_user ('admin', get_password_hash ('admin'), 'Admin', 'admin@admin')
-if db.check_username_is_available ('lltzpp'):
-    db.insert_user ('lltzpp', get_password_hash ('alternate'), 'miohitokiri5474', 'lltzpp@gmail.com')
 
-@app.get ('/')
+if db.check_username_is_available("admin"):
+    db.insert_user("admin", get_password_hash("admin"), "Admin", "admin@admin")
+if db.check_username_is_available("lltzpp"):
+    db.insert_user(
+        "lltzpp", get_password_hash("alternate"), "miohitokiri5474", "lltzpp@gmail.com"
+    )
+
+
+@app.get("/")
 async def read_root():
     return "Welcome to the URL shortener API"
 
-@app.get ("/r/{url}")
-async def get_url (url: str):
-    if db.check_admin_code_is_available (url):
+
+@app.get("/r/{url}")
+async def get_url(url: str):
+    if db.check_admin_code_is_available(url):
         return "This admin url is not exist.\n"
 
-    db.update_click_count (url)
+    db.update_click_count(url)
 
-    return RedirectResponse (db.query (url))
+    return RedirectResponse(db.query(url))
 
-@app.get ("/list/")
-async def list_url (current_user: User = Depends (get_current_active_user)):
-    return db.list (current_user.username)
 
-@app.get ('/whoami/', response_model = User)
-async def read_me (current_user: User = Depends (get_current_active_user)):
+@app.get("/list/")
+async def list_url(current_user: User = Depends(get_current_active_user)):
+    return db.list(current_user.username)
+
+
+@app.get("/whoami/", response_model=User)
+async def read_me(current_user: User = Depends(get_current_active_user)):
     current_user.passwd = b"---"
     return current_user
 
-@app.post ('/token')
-async def user_login (form_data: OAuth2PasswordRequestForm = Depends()):
-    user_info = authenticate_user (form_data.username, form_data.password)
+
+@app.post("/token")
+async def user_login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_info = authenticate_user(form_data.username, form_data.password)
 
     if not user_info:
-        raise HTTPEXception (
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail = "Incorrect username of password",
-            headers = {"WWW-Authenticate": "Bearer"},
+        raise HTTPEXception(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username of password",
+            headers={"WWW-Authenticate": "Bearer"},
         )
 
-    access_token_expires = timedelta (minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token (
-            data = {"sub": user_info.username},
-            expires_delta = access_token_expires
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user_info.username}, expires_delta=access_token_expires
     )
 
     return {"access_token": access_token, "token_type": "bearer"}
 
-@app.post ('/add_url')
-async def adding_url_with_auth_token (url: URLAdminInfo, current_user: User = Depends (get_current_active_user)):
+
+@app.post("/add_url")
+async def adding_url_with_auth_token(
+    url: URLAdminInfo, current_user: User = Depends(get_current_active_user)
+):
     if url.admin_url == "":
         url.admin_url = create_random_url()
-        while not db.check_admin_code_is_available (url.admin_url):
+        while not db.check_admin_code_is_available(url.admin_url):
             url.admin_url = create_random_url()
 
     try:
-        db.insert_url (url.admin_url, url.original_url, current_user.username)
-        return url.admin_url + " -> " + url.original_url + " by. " + current_user.username
+        db.insert_url(url.admin_url, url.original_url, current_user.username)
+        return (
+            url.admin_url + " -> " + url.original_url + " by. " + current_user.username
+        )
     except ValueError as error:
-        return str (error)
+        return str(error)
 
-@app.delete ("/delete_url/{url}")
-async def delete_url_with_auth_token (url: str, current_user: User = Depends (get_current_active_user)):
+
+@app.delete("/delete_url/{url}")
+async def delete_url_with_auth_token(
+    url: str, current_user: User = Depends(get_current_active_user)
+):
     try:
-        db.delete_url (url, current_user.username)
+        db.delete_url(url, current_user.username)
         return url + " is successfully deleted.\n"
     except ValueError as error:
-        return str (error)
+        return str(error)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run (app, host = '0.0.0.0', port = 8000)
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
