4c860c454d99abeb474d4fcdb29cf0fe02fe0a48
miohitokiri5474 <lltzpp>
2024-01-26 15:20:46 +0800

    chore(makefile, main.py): add a makefile
    added a makefile for installation and execute service

--- app/main.py
+++ app/main.py
@@ -300,4 +300,4 @@ def shutdown():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, reload=True, host="0.0.0.0", port=8000)
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
