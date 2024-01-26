23bb48c157a60a2b59cd8641f73517339fff78a0
miohitokiri5474 <lltzpp>
2024-01-26 13:25:39 +0800

    test(tests/test_db.py): added a missing test about delete_user
    As last commit, I added a missing test about delete_url, I also found I diddn't create a test about
    delete_user with database (although I had alreade add a similar test at test_fastapi, but it is
    fine).

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,6 @@
 """
 testing database relate
 """
-import os
-
 from app import db
 
 USERNAME = "lltzpp"
@@ -11,32 +9,14 @@ FULL_NAME = "miohitokiri5474"
 EMAIL = "lltzpp@gmail.com"
 
 
-def clear_db():
-    """clear previous testing data"""
-    if len(db.list_url("lltzpp")) == 1:
-        db.delete_url("main_page", "lltzpp")
-
-    if not db.check_username_is_available("lltzpp"):
-        db.delete_user("lltzpp")
-
-
-def test_build():
-    """testing build_db"""
-    if os.path.exists(db.DB_PATH):
-        os.remove(db.DB_PATH)
-    db.build_db()
-
-
 def test_insert_url_and_check_admin_code():
     """testing insert_url"""
-    clear_db()
     db.insert_url("main_page", "https://miohitokiri5474.github.io/", "lltzpp")
     assert db.check_admin_code_is_available("main_page") is False
 
 
 def test_click_count():
     """testing click count"""
-    clear_db()
     db.insert_url("main_page", "https://miohitokiri5474.github.io/", "lltzpp")
     clicks = db.list_url("lltzpp")[0][2]
     db.update_click_count("main_page")
@@ -45,7 +25,6 @@ def test_click_count():
 
 def test_query():
     """testing querying"""
-    clear_db()
     db.insert_url("main_page", "https://miohitokiri5474.github.io/", "lltzpp")
     assert not db.query("main_page") is None
     assert len(db.list_url("lltzpp")) == 1
@@ -53,14 +32,12 @@ def test_query():
 
 def test_check_insert_user():
     """testing check insert user"""
-    clear_db()
     db.insert_user(USERNAME, PASSWD, FULL_NAME, EMAIL)
     assert db.check_username_is_available("lltzpp") is False
 
 
 def test_update_user_status():
     """testing update_user_status"""
-    clear_db()
     db.insert_user(USERNAME, PASSWD, FULL_NAME, EMAIL)
     db.update_user_status(USERNAME, False)
     assert db.get_user_info("lltzpp")[4] == 0
@@ -70,11 +47,21 @@ def test_update_user_status():
 
 def test_get_passwd():
     """testing get_passwd"""
-    clear_db()
     db.insert_user(USERNAME, PASSWD, FULL_NAME, EMAIL)
     assert db.get_passwd(USERNAME) == PASSWD
 
 
-def test_selalchemy():
-    """testing sqlalchemy"""
-    assert db.sql_alchemy_testing() is None
+def test_delete_url():
+    """testing delete_url"""
+    db.insert_url("main_page", "https://miohitokiri5474.github.io/", "lltzpp")
+    assert db.check_admin_code_is_available("main_page") is False
+    db.delete_url("main_page", "lltzpp")
+    assert db.check_admin_code_is_available("main_page")
+
+
+def test_delete_user():
+    """testing delete_user"""
+    db.insert_user(USERNAME, PASSWD, FULL_NAME, EMAIL)
+    assert db.check_username_is_available("lltzpp") is False
+    db.delete_user(USERNAME)
+    assert db.check_username_is_available("lltzpp")
